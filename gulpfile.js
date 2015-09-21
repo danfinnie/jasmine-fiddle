8d18b7bc6e70ed983d14b56917c25e883ff82618
Dan Finnie <dan@danfinnie.com>
2015-09-20 20:59:03 -0400

    Add ng-annotate to first party dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,23 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var ghPages = require('gulp-gh-pages');
+var ngAnnotate = require('gulp-ng-annotate');
+var merge2 = require('merge2');
 
 gulp.task('concatJs', function () {
-  return gulp.src([
+  var thirdParty = gulp.src([
       'bower_components/ace-builds/src-noconflict/ace.js',
       'bower_components/ace-builds/src-noconflict/ext-language_tools.js',
       'bower_components/ace-builds/src-noconflict/mode-javascript.js',
       "bower_components/angular/angular.js",
       "bower_components/angular-ui-ace/ui-ace.js",
       "bower_components/jasmine-core/lib/jasmine-core/jasmine.js",
-      "bower_components/jasmine-core/lib/jasmine-core/jasmine-html.js",
-      "scripts/*.js"
-      ])
+      "bower_components/jasmine-core/lib/jasmine-core/jasmine-html.js"
+  ]);
+
+  var firstParty = gulp.src('scripts/*.js').pipe(ngAnnotate());
+
+  return merge2(thirdParty, firstParty)
     .pipe(concat('script.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/'));
