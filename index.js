4bd068f2fabd8769946d8602a07b8f0f52b11a3c
Dan Finnie <dan@danfinnie.com>
2015-09-26 14:13:51 -0400

    Rebrand to Jasmine Fiddle

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use("/js-cdd", express.static(path.join(__dirname, 'dist')));
+app.use("/jasmine-fiddle", express.static(path.join(__dirname, 'dist')));
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Server running at http://localhost:3000/jasmine-fiddle', host, port);
 });
